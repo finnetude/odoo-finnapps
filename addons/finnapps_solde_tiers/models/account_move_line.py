b09538cc21426b943a95f6d2821a797ed25f4937
zakaria <zakariaslimi53@gmail.com>
2024-05-02 15:37:37 +0100

    with custom config

--- addons/finnapps_solde_tiers/models/account_move_line.py
+++ addons/finnapps_solde_tiers/models/account_move_line.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api, _
 import re
 import logging
+_logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -18,7 +19,21 @@ class AccountMoveLine(models.Model):
         compute='_getnames',
 
     )
+    
+    residual = fields.Monetary('Résiduel',
+        currency_field='company_currency_id',
+        compute='_compute_residual',store=True)
 
+    cumulated_residual = fields.Monetary('Résiduel cumulé', store=False,
+        currency_field='company_currency_id',
+        # compute='_compute_cumulated_residual',
+        help="Residual cumulé en fonction du domaine et de l'ordre choisi dans la vue.")
+
+    cumulated_balance = fields.Monetary(string='Solde', store=False,
+        currency_field='company_currency_id',
+        compute='_compute_cumulated_balance',
+        help="Solde cumulé en fonction du domaine et de l'ordre choisi dans la vue.")
+    
     def test(self):
         domain = self.env.context.get('default_domain_record')
 
@@ -58,19 +73,7 @@ class AccountMoveLine(models.Model):
                 i.ref_pc = i.move_id.name
         return
         
-    residual = fields.Monetary('Résiduel',
-        currency_field='company_currency_id',
-        compute='_compute_residual',store=True)
 
-    cumulated_residual = fields.Monetary('Résiduel cumulé', store=False,
-        currency_field='company_currency_id',
-        # compute='_compute_cumulated_residual',
-        help="Residual cumulé en fonction du domaine et de l'ordre choisi dans la vue.")
-
-    cumulated_balance = fields.Monetary(string='Solde', store=False,
-        currency_field='company_currency_id',
-        compute='_compute_cumulated_balance',
-        help="Solde cumulé en fonction du domaine et de l'ordre choisi dans la vue.")
 
     previous_solde = fields.Monetary(string='Solde antérieur',
                                   compute="_compute_previous_solde",)
@@ -98,9 +101,9 @@ class AccountMoveLine(models.Model):
         order = self.env.context.get('order_cumulated_balance')
 
         aml = self.env['account.move.line'].search([('id','in',self.ids)], order=order)
+        _logger.info(self.env['account.move.line'])
         n = aml[0].cumulated_balance - (aml[0].debit - aml[0].credit)
         for rec in aml:
-
             rec.previous_solde = n
             n = rec.cumulated_balance
 
@@ -133,7 +136,6 @@ class AccountMoveLine(models.Model):
         self.env.cr.execute(sql, where_clause_params)
         fetched = self.env.cr.fetchall()
         result = {r[0]: r[1] for r in fetched}
-
 
         domain_all = []
         for count, elem in enumerate(domain):
@@ -178,7 +180,8 @@ class AccountMoveLine(models.Model):
     def _compute_cumulated_balance(self):
         domain = self.env.context.get('domain_cumulated_balance')
         order = self.env.context.get('order_cumulated_balance')
-       
+
+
         if not domain:
             # Nous ne venons pas de search_read, nous ne sommes donc pas dans une vue de liste, donc cela n'a aucun sens de calculer le solde cumulé
             for record in self:
@@ -187,33 +190,32 @@ class AccountMoveLine(models.Model):
 
         # get the where clause
         result ,result_all = self.compute_cumulated_balance(order, domain)
+
         if result != result_all :
             keys = []
             for k in result:
                 keys.append(k)
 
-            move_lines = self.env['account.move.line'].search([('id','in',keys)],order="date asc")
-
+            move_lines = self
+            # _logger.info(move_lines)
+            # move_lines = self.env['account.move.line'].search([('id','in',keys)],order="date asc")
+            # _logger.info(move_lines)
             first = True
 
             epsilon = 0
-
             for record in move_lines:
-               
+
                 if first:
-                    if record.id in result_all:
-                       
+                    if record.id in result_all:     
                         epsilon = result_all[record.id] - result[record.id] 
                         record.cumulated_balance = result_all[record.id] 
                     else:
                         epsilon = 0
-                        
                         record.cumulated_balance = result[record.id] 
                     first = False
                 else :
                     record.cumulated_balance = epsilon + result[record.id]
         else:
-
             for record in self:
                 record.cumulated_balance = result[record.id]
 
--- addons/finnapps_solde_tiers/models/res_partner.py
+++ addons/finnapps_solde_tiers/models/res_partner.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 import logging
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -17,31 +18,41 @@ class ResPartner(models.Model):
         string="Solde",
         compute='get_the_solde',
         store=True,
-
     )
 
     def get_solde(self):
         sudo = self.sudo()
-        sql_receivable = "SELECT id FROM account_account WHERE account_type in ('asset_receivable','asset_payable')"
+        sql_receivable = "SELECT id FROM account_account WHERE account_type in ('asset_receivable','liability_payable')"
         sql_posted = "SELECT id FROM account_move WHERE state = 'posted'"
+        final_sql = sql_receivable 
         for record in self:
             ids = tuple(record.env['account.move.line'].search([('partner_id','=',record.id),('move_id.state','=', 'posted')]).ids)
             if ids :
                 if len(ids) == 1:
-                    sql = "select SUM(debit - credit) as solde FROM account_move_line WHERE account_move_line.id IN ({}) and account_id IN ({}) ".format(ids[0], sql_receivable)
+                    sql = "select SUM(debit - credit) as solde FROM account_move_line WHERE account_move_line.id IN ({}) and account_id IN ({}) ".format(ids[0], final_sql)
                 else:
-                    sql = "select SUM(debit - credit) as solde FROM account_move_line WHERE account_move_line.id IN {} and account_id IN({}) ".format(ids, sql_receivable)
+                    sql = "select SUM(debit - credit) as solde FROM account_move_line WHERE account_move_line.id IN {} and account_id IN({}) ".format(ids, final_sql)
 
 
                 self.env.cr.execute(sql)
                 aml = self.env.cr.dictfetchall()
-
+                # _logger.info("zakkkk 2")
+                # _logger.info()
+                # _logger.info(aml[0])
                 return aml[0]['solde']
         
         return 0
-
-
+    
+    # @api.model
+    # def get_view(self, view_id=None, view_type='form', **options):
+    #     res = super(ResPartner, self).get_view(view_id,view_type,**options)
+    #     all_ids = self.env['res.partner'].search([('active',"=",True)])
+    #     for id_item in all_ids:
+    #         id_item.get_the_solde()
+    #     return res
+    
     @api.depends('move_line_ids')
     def get_the_solde(self):
-        self.solde = self.get_solde()
+        value = self.get_solde()
+        self.solde = value
 
